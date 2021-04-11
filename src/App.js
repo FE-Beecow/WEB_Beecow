e23a2d1083b9c059493974bbed181fc779683cfc
LAPTOP-L3O45KPU\HP <hahuuhung0102@gmail.com>
2021-04-11 17:52:47 +0700

    Hung - Update: Navbar + Dropdown user

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import './App.css';
 import React, { Component } from 'react'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from "react-router-dom";
 import Home from './components/home';
 import FormLogin from './components/login/formLogin';
 import LoginModal from './components/login/modalLogin';
 import FormRegister from './components/register/formRegister';
+import Navbar from './components/Navbar';
+import home1 from './components/home1';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <div className="App">
-            <Route path="/" exact component={Home} />
+      <Router>
+        <Navbar />
+          <Switch>
+          <Route path="/" exact component={home1} />
             <Route path="/login" component={LoginModal} />
             <Route path="/register" component={FormRegister} />
-          </div>
-        </Router>
-      </div>
+          </Switch>
+          {/* <Router>
+            <div className="App">
+              <Route path="/" exact component={Home} />
+              <Route path="/login" component={LoginModal} />
+              <Route path="/register" component={FormRegister} />
+            </div>
+          </Router> */}
+      </Router>
     )
   }
 }
